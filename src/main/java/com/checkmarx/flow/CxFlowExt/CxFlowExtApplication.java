7ec9f3c248988e888a1720d17d8f2790b81118b9
Ken McDonald <ken.mcdonald@checkmarx.com>
2020-04-06 19:58:13 -0400

    Initial Commit

---
+++ src/main/java/com/checkmarx/flow/CxFlowExt/CxFlowExtApplication.java
@@ -0,0 +1,9 @@
+package com.checkmarx.flow.CxFlowExt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxFlowExtApplication {
+
+}
---
+++ src/test/java/com/checkmarx/flow/CxFlowExt/CxFlowExtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkmarx.flow.CxFlowExt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CxFlowExtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
