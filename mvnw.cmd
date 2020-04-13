830b013a0c2ed7ebbbdd12fdf5a47b6243bbe7cd
Ken McDonald <ken@custodela.com>
2020-04-12 21:35:12 -0400

    Update steps for circleci

--- mvnw.cmd
+++ mvnw.cmd
@@ -37,10 +37,6 @@
 @echo off
 @REM set title of command window
 title %0
-@REM Download and install the cxflow release
-set /p CX_FLOW_VERSION=<cxflow.version
-curl -L https://github.com/checkmarx-ts/cx-flow/releases/download/%CX_FLOW_VERSION%/cx-flow-%CX_FLOW_VERSION%.jar --output cx-flow-%CX_FLOW_VERSION%.jar
-mvn install:install-file -Dfile=cx-flow-%CX_FLOW_VERSION%.jar -DgroupId=com.checkmarx.flow -DartifactId=cx-flow -Dversion=%CX_FLOW_VERSION% -Dpackaging=jar -DgeneratePom=true
 @REM enable echoing by setting MAVEN_BATCH_ECHO to 'on'
 @if "%MAVEN_BATCH_ECHO%" == "on"  echo %MAVEN_BATCH_ECHO%
 
